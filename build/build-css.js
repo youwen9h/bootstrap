70a8bf98c5c654c003bbf229badbd24f165f2d72
XhmikosR <xhmikosr@gmail.com>
2021-01-01 13:09:39 +0200

    WIP2

--- build/build-css.js
+++ build/build-css.js
@@ -2,48 +2,58 @@
 
 'use strict'
 
-const fs = require('fs')
+const fs = require('fs/promises')
 const path = require('path')
 const postcss = require('postcss')
 const glob = require('glob')
 const sass = require('sass')
 const autoprefixer = require('autoprefixer')
-const fiber = require('fibers')
 const Cleancss = require('clean-css')
 
 const postcssPlugins = [
   autoprefixer({ cascade: false })
 ]
 
-const files = [
+const FILES = [
   'bootstrap',
   'bootstrap-grid',
   'bootstrap-reboot',
   'bootstrap-utilities'
 ]
 
-files.forEach(filename => {
-  sass.render({
+async function processFile(filename) {
+  const sassOptions = {
     file: path.resolve(`./scss/${filename}.scss`),
-    fiber,
     outputStyle: 'expanded',
     sourceMap: true,
     sourceMapContents: true,
     outFile: `./dist/css/${filename}.css`
-  }, (error, css) => {
-    // Fix postcss to use and output sourcemaps
-    postcss(postcssPlugins)
-      .process(css.css, {
-        from: `./dist/css/${filename}.css`,
-        map: `./dist/css/${filename}.css.map`
-      })
-      .then(result => {
-        fs.writeFile(`./dist/css/${filename}.css`, result.css, () => true)
-        fs.writeFile(`./dist/css/${filename}.css.map`, result.map.toString(), () => true)
-        fs.writeFile(`./dist/css/${filename}.min.css`, new Cleancss().minify(result.css).styles, () => true)
-      })
-  })
-})
+  }
+
+  const css = await sass.renderSync(sassOptions)
+
+  // Fix postcss to use and output sourcemaps
+  const processedCss = await postcss(postcssPlugins)
+    .process(css.css, {
+      from: `./dist/css/${filename}.css`,
+      map: `./dist/css/${filename}.css.map`
+    })
+
+  const minifiedCss = await new Cleancss().minify(processedCss.css).styles
+
+  await fs.writeFile(`./dist/css/${filename}.css`, processedCss.css)
+  await fs.writeFile(`./dist/css/${filename}.css.map`, processedCss.map.toString())
+  await fs.writeFile(`./dist/css/${filename}.min.css`, minifiedCss)
+}
+
+(async () => {
+  try {
+    await Promise.all(FILES.map(file => processFile(file)))
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+})()
 
 // Prefix examples
 glob('./site/content/**/*.css', {}, (error, files) => {
